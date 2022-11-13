fb5814ba39bc7176d26f343738e2e73740a2f6df
Alexandre D <99996676+AlexandreDonisete@users.noreply.github.com>
2022-11-13 18:57:08 -0300

    Add files via upload

---
+++ Exercícios 11/Exercicio11_01/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio11_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double media = 0, acimaMedia = 0;
+            double[] notas = new double[10];
+
+            // Digitar as notas da turma.
+            Console.WriteLine("Digite as notas dos 10 alunos: ");
+            for(i = 0; i < 10; i++)
+            {
+                notas[i] = double.Parse(Console.ReadLine());
+            };
+
+            Console.WriteLine();
+
+            // Efetuar a média das notas da turma.
+            for (i = 0; i < 10; i++)
+            {
+                media += (notas[i]) / 10;
+            };
+            Console.WriteLine("A média da turma é: " + media);
+
+            Console.WriteLine();
+
+            // Testar se as notas da turma estão acima da média ou não. Caso esteja, acrescentar +1 na váriável "acimaMedia".
+            for (i = 0; i < 10; i++)
+            {
+                if(notas[i] > media)
+                {
+                    acimaMedia += 1;
+                }
+            };
+
+            // Exibir as notas da turma.
+            Console.WriteLine("Notas da turma: ");
+            for (i = 0; i < 10; i++)
+            {
+                if (notas[i] > media)
+                {
+                    Console.WriteLine("Está nota está acima da média calculada: " + notas[i]);
+                } else
+                {
+                    Console.WriteLine("Está nota não está acima da média calculada: " + notas[i]);
+                }
+                
+            };
+
+            Console.WriteLine();
+
+            Console.WriteLine("Houveram {0} notas acima da média calculada", acimaMedia);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercícios 11/Exercicio11_02/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio11_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double[] Q = new double[10];
+            double menor = 0, maior = 0, maiorPosicao = 0;
+
+            // Entre com os 10 valores do array Q, todos maiores que 0.
+            Console.WriteLine("Valores do array Q: ");
+            for (i = 0; i < Q.Length; i++)
+            {
+                Console.Write((i + 1) + "º número: ");
+                Q[i] = double.Parse(Console.ReadLine());
+
+                if (Q[i] < 0)
+                {
+                    Console.WriteLine("Não digite números menores que zero!");
+                    Console.ReadKey();
+                    return;
+                }
+
+            };
+
+            Console.WriteLine("");
+
+            //* Criar um loop que, compara o valor do índice atual com o valor da variável "menor" ou "maior".
+            //* Caso o valor do índice atual seja menor que a variável "menor", o valor da variável será substituido pelo valor do índice atual.
+            //* Caso o valor do índice atual seja maior que a variável "maior", o valor da variável será substituido pelo valor do índice atual,
+            //* e o valor da variável "maiorPosicao" será substituído pelo índice atual do array. */
+            for (i = 0; i < Q.Length; i++)
+            {
+                if (Q[i] < menor)
+                {
+                    menor = Q[i];
+
+                } else if (Q[i] > maior)
+                {
+                    maior = Q[i];
+                    maiorPosicao = i;
+                };
+            };
+
+            Console.WriteLine("O maior número é: " + maior);
+            Console.WriteLine("O número está na " + maiorPosicao + "ª posição do array.");
+            Console.ReadKey();
+        }
+    }
+}
